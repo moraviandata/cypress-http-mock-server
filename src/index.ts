7f2c86c10f12b0445f7d3d02e33c533f4b69b70f
Martin Ptáček <martin.matematik@gmail.com>
2023-09-01 15:02:53 +0200

    add cli arguments parsing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
-
-
-// export default app
-
+import { parse } from 'ts-command-line-args';
+import yargs from 'yargs/yargs'
 import app from "./app"
 
-const PORT : number = 3000;
+const DEFAULT_PORT : number = 3000;
 
-app.listen(PORT, ()=> console.log('running application now'))
+interface IArguments {
+    port?: number,
+    help?: boolean
+}
+
+
+function main(){
+    let args : IArguments
+    
+    try{
+        args = parse<IArguments>(
+            {
+                port: { type: Number, optional: true, alias: 'p'},
+                help: { type: Boolean, optional: true, alias: 'h', description: 'Prints this usage guide', defaultValue: false},
+            },
+            {
+                helpArg: 'help',
+                headerContentSections: [{ 
+                    header: 'cypress-http-mock-server', 
+                    content: 'This is a simple application that runs a http mock server, useful for testing SSR fetches in frameworks like Next.js wit cypress.' 
+                }],
+                footerContentSections: [{ header: '', content: `Copyright: moraviandata s.r.o.` }],
+            },
+        )
+    }catch(e){
+        console.log("Could not parse command line arguments. Run with -h to view the manual.")
+        return
+    }
+    
+    const port : number = args.port ?? DEFAULT_PORT
+    
+    app.listen(port, ()=> console.log('Started successfuly on port ' + port.toString()))
+}
+
+main()
